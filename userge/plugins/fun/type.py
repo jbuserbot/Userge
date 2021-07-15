5dc7f3904e64431d85cf1fe4a9e72291997c8654
James Bond <86193831+jbuserbot@users.noreply.github.com>
2021-07-15 10:12:04 +0530

    Update type.py

--- userge/plugins/fun/type.py
+++ userge/plugins/fun/type.py
@@ -7,25 +7,3 @@
 # All rights reserved.
 
 from userge import userge, Message
-
-
-@userge.on_cmd("type", about={
-    'header': "Simulate a typewriter",
-    'usage': "{tr}type [text]"})
-async def type_(message: Message):
-    text = message.input_str
-    if not text:
-        await message.err("for me - texta sethi anupuda")
-        return
-    typing_symbol = '|'
-    old_text = ''
-    await message.edit(typing_symbol)
-    for character in text:
-        if message.process_is_canceled:
-            await message.edit("`process cancelled`")
-            break
-        print("ololol")
-        old_text += character
-        typing_text = old_text + typing_symbol
-        await message.try_to_edit(typing_text, sudo=False)
-        await message.try_to_edit(old_text, sudo=False)
